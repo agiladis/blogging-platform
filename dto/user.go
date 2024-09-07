b27905a071f2d131f37c9c06c0f906c000b0f130
Agil Adi Saputro <agiladisaputro@gmail.com>
2024-09-08 02:42:21 +0700

    [UPDATE] username field to be unique

--- dto/user.go
+++ dto/user.go
@@ -12,6 +12,6 @@ type UserResponseDTO struct {
 }
 
 type UserLoginDTO struct {
-	Email    string `json:"email" binding:"required"`
+	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
