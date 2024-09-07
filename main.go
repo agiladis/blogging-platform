53aa9a417a1e4ec3d3ff68983681ba08fc9fb3bd
Agil Adi Saputro <agiladisaputro@gmail.com>
2024-09-07 22:01:30 +0700

    [ADD] model

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import "blogging-platform/config"
+
 func main() {
+	// initiate DB
+	config.InitDB()
+
 	
 }
---
+++ model/blogPost.go
@@ -0,0 +1,13 @@
+package model
+
+import (
+	"gorm.io/gorm"
+)
+
+type BlogPost struct {
+	gorm.Model
+	// ID uint `json:"id" gorm:"primaryKey"`
+	Title string `json:"title" gorm:"not null;type:varchar(30)"`
+	Content string `json:"content" gorm:"not null;type:text"`
+	UserID uint `json:"user_id" gorm:"not null"`
+}
