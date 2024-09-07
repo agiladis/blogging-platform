2b3fcf3a717fbccce9256bcaab40a07159c20eda
Agil Adi Saputro <agiladisaputro@gmail.com>
2024-09-08 04:36:10 +0700

    [ADD] router

---
+++ router/router.go
@@ -0,0 +1,27 @@
+package router
+
+import (
+	"blogging-platform/controller"
+	"blogging-platform/repository"
+	"blogging-platform/service"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func StartServer(DB *gorm.DB) *gin.Engine {
+	// Setup repository, service, dan controller
+	userRepository := repository.NewUserRepository(DB)
+	userService := service.NewUserService(userRepository)
+	userController := controller.NewUserController(userService)
+
+	app := gin.Default()
+
+	// Routing
+	userRouter := app.Group("/users")
+	{
+		userRouter.POST("/register", userController.Register)
+	}
+
+	return app
+}
