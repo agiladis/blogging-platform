dde050d7ff8d415bb14599e620383ee7c9c289bf
Agil Adi Saputro <agiladisaputro@gmail.com>
2024-09-08 03:22:57 +0700

    [ADD] layer controller to handle register user

---
+++ controller/user_controller.go
@@ -0,0 +1,44 @@
+package controller
+
+import (
+	"blogging-platform/dto"
+	"blogging-platform/service"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userController struct {
+	userService service.UserService
+}
+
+func NewUserController(userService service.UserService) *userController {
+	return &userController{userService}
+}
+
+func (uc *userController) Register(ctx *gin.Context) {
+	var userRegisterDTO dto.UserRegisterDTO
+
+	if err := ctx.ShouldBindJSON(&userRegisterDTO); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"status": "error",
+			"message": err.Error(),
+		})
+		return
+	}
+
+	user, err := uc.userService.Register(userRegisterDTO)
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"status": "error",
+			"message": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{
+		"status": "success",
+		"message": "user created",
+		"data": user,
+	})
+}
