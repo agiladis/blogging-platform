97f769e07d268ec88aef960bee378be04c4ef3e4
Agil Adi Saputro <agiladisaputro@gmail.com>
2024-09-08 19:32:00 +0700

    [UPDATE] unused comment on user and blogPost model

--- model/blogPost.go
+++ model/blogPost.go
@@ -6,7 +6,6 @@ import (
 
 type BlogPost struct {
 	gorm.Model
-	// ID uint `json:"id" gorm:"primaryKey"`
 	Title string `json:"title" gorm:"not null;type:varchar(30)"`
 	Content string `json:"content" gorm:"not null;type:text"`
 	UserID uint `json:"user_id" gorm:"not null"`
